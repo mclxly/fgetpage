5c8c6820fa523d76593e4597133fa399f90ca133
colin.lin <legoo8@qq.com>
2013-05-09 12:32:14 +0800

    added start item# as parameter

--- write_urls.php
+++ write_urls.php
@@ -1,4 +1,8 @@
 <?php
+/*
+	usage 1: php write_urls.php %total_url_num%
+	usage 2: php write_urls.php %total_url_num% %start_item_no%
+*/
 $myFile = "test_urls.txt";
 $fh = fopen($myFile, 'w') or die("can't open file");
 
@@ -7,6 +11,9 @@ $start_no = 652405;
 
 if (isset($argv[1]))
 	$counter = $argv[1];
+	
+if (isset($argv[2]))
+	$start_no = $argv[2];	
 	
 for ($i=0; $i<$counter; ++$i) {
 	++$start_no;
